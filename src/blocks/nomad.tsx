91de419663c1429f90e3afa33dfc4e5b4a12ad51
Inna Batalova <batalovainna97@gmail.com>
2022-10-11 18:53:24 +0300

    Create: added nomad styles part 2

--- src/blocks/nomad.tsx
+++ src/blocks/nomad.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import ChannelItem from '../components/nomad/channels-item/channels-item'
 import FriendsItem from '../components/nomad/friends-item/friends-item'
@@ -15,7 +15,32 @@ import ChatIcon from '../static/img/chat-icon.svg'
 
 function Nomad() {
 
-    const [count] = useState(0);
+    const channels: JSX.Element[] = [
+            <ChannelItem value="# general" /> ,
+            <ChannelItem value="# support"/>,
+            <ChannelItem value="# marketing"/>,
+            <ChannelItem value="# thailand"/>,
+            <ChannelItem value="# bali"/>,
+            <ChannelItem value="# poland"/>,
+            <ChannelItem value="# australia"/>,
+            <ChannelItem value="# jobs"/>,
+            <ChannelItem value="# startups"/>,
+            <ChannelItem value="# italy"/>,
+            <ChannelItem value="# freelance"/> ];
+
+    const channelsLenght: number = channels.length;
+    const listChannels: JSX.Element[] = channels.map((number) =><>{number}</>);
+
+    const friends: JSX.Element[] = [
+            <FriendsItem name="Orlando Diggs" src={AvatarImg1} class="online" />,
+            <FriendsItem name="Carmen Velasco" src={AvatarImg2} class="online" />,
+            <FriendsItem name="Marie Jensen" src={AvatarImg3} />,
+            <FriendsItem name="Alex Lee" src={AvatarImg4} />,
+            <FriendsItem name="Leo Gill" src={AvatarImg5} />,
+            <FriendsItem name="Britney Cooper" src={AvatarImg6} /> ];
+
+    const friendsLenght: number = friends.length;
+    const listFriends: JSX.Element[] = friends.map((number) =><>{number}</>);
     
 
     return (
@@ -34,33 +59,20 @@ function Nomad() {
         <div className="nomad-channel">
 
             <div className="nomad-channel-wrap">
-                <h4 className="channel__title">Channel</h4>
-                <span className="channel__counter">{count}</span>
+                <h4 className="nomad-channel__title">Channel</h4>
+                <span className="nomad-channel__counter">{channelsLenght}</span>
             </div>
+        
+        <div className="channel-wrapper">  {listChannels}  </div>  
 
-            <div className="channel-wrapper">
-            <ChannelItem value="# general" /> 
-            <ChannelItem value="# support"/>
-            <ChannelItem value="# marketing"/>
-            <ChannelItem value="# thailand"/>
-            <ChannelItem value="# bali"/>
-            <ChannelItem value="# poland"/>
-            <ChannelItem value="# australia"/>
-            <ChannelItem value="# jobs"/>
-            <ChannelItem value="# startups"/>
-            <ChannelItem value="# italy"/>
-            <ChannelItem value="# freelance"/>
-            </div>
         </div>
 
-        <div className="friends-wrapper">
-            <FriendsItem name="Orlando Diggs" src={AvatarImg1} class="online" />
-            <FriendsItem name="Carmen Velasco" src={AvatarImg2} class="online" />
-            <FriendsItem name="Marie Jensen" src={AvatarImg3} />
-            <FriendsItem name="Alex Lee" src={AvatarImg4} />
-            <FriendsItem name="Leo Gill" src={AvatarImg5} />
-            <FriendsItem name="Britney Cooper" src={AvatarImg6} />
-        </div>
+            <div className="nomad-friends-wrap">
+                <h4 className="friends__title">Friends</h4>
+                <span className="friends__counter">{friendsLenght}</span>
+            </div>
+
+        <div className="friends-wrapper">  {listFriends}  </div>
 
         </div>
     </div>
