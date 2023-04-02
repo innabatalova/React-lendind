9f47a0990dc48ff946a9a5cbf6f835da2b9603c5
Inna Batalova <batalovainna97@gmail.com>
2023-04-02 22:20:29 +0300

    Test path 2

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 import "./scss/style.scss"
 
 // import Layout from "./blocks/Layout"
-import Layout from "./opt/build/repo/src/blocks/Layout"
+import Layout from "/opt/build/repo/src/blocks/Layout"
 
 
 ReactDOM.render(<Layout/>, document.getElementById('root'))
