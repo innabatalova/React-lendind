1e8d3ba7ffe6aa3a45845456642e2e497c866e7d
Inna Batalova <batalovainna97@gmail.com>
2023-05-09 15:05:24 +0300

    Merge branch 'main' of https://github.com/innabatalova/React-lendind

--- src/blocks/BioBlock/BioBlock.tsx
+++ src/blocks/BioBlock/BioBlock.tsx
@@ -12,8 +12,7 @@ import IconTwitter from "../../static/img/bio-icon-twitter.svg"
 import IconInstagram from "../../static/img/bio-icon-instagram.svg"
 import IconIn from "../../static/img/bio-icon-in.svg"
 
-const BioBlock = ({classVisible, clicked}: IVisibleProps) => {
-  
+const BioBlock = ({ classVisible, clicked }: IVisibleProps) => {  
   return (
     <div className={`bio ` + classVisible} onClick={clicked}>
       <img src={PhotoMain} className="bio-main" alt="photo profile" />
